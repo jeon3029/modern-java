cc11806a5e4d910528696b77d6ab0d542b059809
taejoon.jeon <taejoon.jeon@navercorp.com>
2022-10-02 21:29:03 +0900

    add lambda sample

---
+++ practice/prac/src/main/java/chap3/Lambda.java
@@ -0,0 +1,29 @@
+package chap3;
+
+public class Lambda {
+	public static void main(String[] args) {
+		System.out.println("Lambda Examples");
+
+		//save lambda
+		Runnable r1 = ()-> System.out.println("Hello World 1");
+
+		//anonymous class
+		Runnable r2 = new Runnable() {
+		 @Override
+		 public void run() {
+			 System.out.println("Hello World 2");
+		 }
+		};
+		process(r1);
+		process(r2);
+
+		//직접 전달
+		process(()-> System.out.println("Hello World 3"));
+
+		r1.run();
+		r2.run();
+	}
+	public static void process(Runnable r) {
+		r.run();
+	}
+}
