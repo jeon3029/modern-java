4f7e98e0678a8f7eeacb36e78a725d1683151063
taejoon.jeon <taejoon.jeon@navercorp.com>
2022-10-02 16:42:13 +0900

    lambda, streaming sample

--- practice/prac/src/main/java/chap1/Filtering.java
+++ practice/prac/src/main/java/chap1/Filtering.java
@@ -1,24 +1,56 @@
 package chap1;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
 
 public class Filtering {
+	//init sample data
+	static List<Apple> inventory = Arrays.asList(
+			new Apple(80, "green"),
+			new Apple(155, "green"),
+			new Apple(120, "red")
+	);
+
 	public static void main(String[] args) {
 		behaviorParameters();
+		lambda();
+		streaming();
+	}
+
+	private static void streaming() {
+		System.out.println("streaming parallel");
+
+		System.out.println("--------------------------------");
+		System.out.println("stream sequential");
+		List<Apple> heavyApples = inventory.stream().filter((Apple a)-> a.getWeight() > 150).collect(toList());
+		System.out.println(heavyApples);
+		System.out.println("stream parallel");
+		List<Apple> heavyApples2 = inventory.parallelStream().filter((Apple a)-> a.getWeight() > 150).collect(toList());
+		System.out.println(heavyApples2);
+	}
+
+	private static void lambda() {
+		System.out.println("pridicate method");
+		List<Apple> greenApples = filterApples(inventory, Filtering::isGreenApple);
+		System.out.println("green(color=green)");
+		System.out.println(greenApples);
+
+
+		List<Apple> heavyApples = filterApples(inventory, Filtering::isHeavyApple);
+
+		System.out.println("heavy(weight>150)");
+		System.out.println(heavyApples);
+
+		List<Apple> filterLambda = filterApples(inventory, (Apple a) -> a.getWeight()>150 || "red".equals(a.getColor()));
+		System.out.println("lambda : heavy or red(>150 or RED)");
+		System.out.println(filterLambda);
+
+
 	}
 
 	private static void behaviorParameters() {
-		//init sample data
-		List<Apple> inventory = Arrays.asList(
-				new Apple(80, "green"),
-				new Apple(155, "green"),
-				new Apple(120, "red")
-		);
 		//sample data print
 		System.out.println("before sort : " + inventory);
 
@@ -37,7 +69,6 @@ public class Filtering {
 		inventory.sort(comparing(Apple::getWeight));
 		System.out.println("after sort : " + inventory);
 	}
-
 
 	public static class Apple {
 
@@ -71,6 +102,45 @@ public class Filtering {
 			return String.format("Apple{color='%s', weight=%d}", color, weight);
 		}
 
+	}
+
+	public static List<Apple> filterGreenApples(List<Apple> inventory) {
+		List<Apple> result = new ArrayList<>();
+		for (Apple apple : inventory) {
+			if ("green".equals(apple.getColor())) {
+				result.add(apple);
+			}
+		}
+		return result;
+	}
+
+	public static List<Apple> filterHeavyApples(List<Apple> inventory) {
+		List<Apple> result = new ArrayList<>();
+		for (Apple apple : inventory) {
+			if (apple.getWeight() > 150) {
+				result.add(apple);
+			}
+		}
+		return result;
+	}
+
+	public static boolean isGreenApple(Apple apple) {
+		return "green".equals(apple.getColor());
+	}
+
+	public static boolean isHeavyApple(Apple apple) {
+		return apple.getWeight() > 150;
+	}
+
+	private interface Predicate<T>{
+		boolean test(T t);
+	}
+	private static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p) {
+		List<Apple> result = new ArrayList<>();
+		inventory.forEach(apple->{
+			if(p.test(apple)) result.add(apple);
+		});
+		return result;
 	}
 }
 
